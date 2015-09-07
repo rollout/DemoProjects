6c2ee0b24eac680d6a2bf7487f848cc5ac668b14
Sergey Ilyevsky <sergey@rollout.io>
2015-09-07 12:36:10 +0300

    Added MissingXibConnections demo project

---
+++ MissingXibConnections/MissingXibConnections/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MissingXibConnections
+//
+//  Created by Sergey Ilyevsky on 9/7/15.
+//  Copyright (c) 2015 Rollout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MissingXibConnections/MissingXibConnections/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  MissingXibConnections
+//
+//  Created by Sergey Ilyevsky on 9/7/15.
+//  Copyright (c) 2015 Rollout. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+//- (IBAction)handleButtonClicked:(id)sender
+//{
+//    
+//}
+
+@end
